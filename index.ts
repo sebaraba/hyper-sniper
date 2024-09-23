39ee4e2ec4ea25ad3cd20dea50cffadf4fdc8e1a
rabas <rabas@haufe.com>
2024-09-23 23:40:55 +0300

    chore: refactore

--- index.ts
+++ index.ts
@@ -1,50 +1,70 @@
 import {Hyperliquid, Tif} from "hyperliquid";
 
+// Initialize the SDK (replace with your actual private key and other necessary parameters)
+const tif: Tif = "Gtc";
+const coin = "PURR-SPOT";
+const private_key = "0x3524c94b436e946a58ab4fc3b582427f94190ce476a4b97eecc89cc007edee32";
+const user_address = "0xC4C654EBd6124ec719eDc71Aa1d4266e2389e0F0";
+const sdk = new Hyperliquid(private_key, false); // false for mainnet, true for testnet
+const buyRequests: any = [];
 
 async function testExchangeAPI() {
-  // Initialize the SDK (replace with your actual private key and other necessary parameters)
-  const private_key = "<priv-key>";
-  const user_address = "0xC4C654EBd6124ec719eDc71Aa1d4266e2389e0F0";
-  const sdk = new Hyperliquid(private_key, false); // false for mainnet, true for testnet
-  const tif: Tif = "Gtc";
-  const coin = "YUM-SPOT";
-  const buyRequests = [1];
-
   try {
+    const orderRequest = {
+      coin: coin,
+      is_buy: true,
+      sz: 100,
+      limit_px: 0.3,
+      order_type: { limit: { tif: tif } },
+      reduce_only: false
+    };
 
-    console.log("Testing ExchangeAPI endpoints:");
-    while(true) {
-      for (let i = 0; i < 3; i++) {
-        const orderRequest = {
-          coin: coin,
-          is_buy: true,
-          sz: 100,
-          limit_px: 0.1 + (0.01 * i),
-          order_type: { limit: { tif: tif } },
-          reduce_only: false
-        };
-
-        console.log(`\nPlacing Order ${i + 1}: date: ${new Date().toISOString()}`);
-        // buyRequests.push(sdk.exchange.placeOrder(orderRequest));
-      }
-
-      await new Promise(resolve => setTimeout(resolve, 1000));
-    }
+    const result = await sdk.exchange.placeOrder(orderRequest);
+    console.log("Order placed successfully:", result);
   } catch (error) {
     console.error("An error occurred:", error);
   } finally {
     // rl.close();
   }
+}
 
-  Promise.allSettled(buyRequests).then((results) => {
-    results.forEach((result, i) => {
-      if (result.status === "fulfilled") {
-        console.log(`Order ${i + 1} placed successfully:`, result.value);
-      } else {
-        console.error(`Order ${i + 1} failed:`, result.reason);
-      }
+const getBatchRequests = async (coin: string) => {
+  for (let i = 0; i < 3; i++) {
+    const orderRequest = {
+      coin: coin,
+      is_buy: true,
+      sz: 100,
+      limit_px: 0.3 + (0.01 * i),
+      order_type: { limit: { tif: tif } },
+      reduce_only: false
+    };
+
+    console.log(`\nPlacing Order ${i + 1}: date: ${new Date().toISOString()}`);
+    const result = await buyRequests.push(sdk.exchange.placeOrder(orderRequest));
+    console.log(`Order ${i + 1} placed successfully:`, JSON.stringify(result));
+  }
+
+  return buyRequests;
+}
+
+const runBatchRequestsWithTimeout = async (timeout: number) => {
+  console.log("Testing ExchangeAPI endpoints:");
+  while(true) {
+    for (let i = 0; i < 3; i++) {
+      buyRequests.push(getBatchRequests(coin));
+    }
+
+    Promise.allSettled(buyRequests).then((results) => {
+      results.forEach((result, i) => {
+        if (result.status === "fulfilled") {
+          console.log(`Order ${i + 1} placed successfully:`, result.value);
+        } else {
+          console.error(`Order ${i + 1} failed:`, result.reason);
+        }
+      });
     });
-  });
+    await new Promise(resolve => setTimeout(resolve, timeout));
+  }
 }
 
 // testCustomExchangeAPI();
